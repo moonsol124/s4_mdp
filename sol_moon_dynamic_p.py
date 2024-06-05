7d97cce631ead46b4e8edb804cd2e3ebfb9c0669
moonsol124 <moonsol124@gmail.com>
2024-06-05 12:31:23 +0200

    f

--- sol_moon_dynamic_p.py
+++ sol_moon_dynamic_p.py
@@ -12,78 +12,6 @@ import pprint as p
 # as the level increases, the following changes:
 # probability of winning and losing 
 
-class MDP:
-    def __init__(self, states, actions, transitions=None, rewards=None, gamma=0.9, eps=1e-6, T=100):
-        self.states = states
-        self.actions = actions
-        self.transitions = transitions
-        self.rewards = rewards
-        self.gamma = gamma
-        self.eps = eps
-        self.T = T
-
-        self.currentState = states[0]
-        self.stepsTaken = 0
-
-    def reset(self):
-        self.currentState = self.states[0]
-        self.stepsTaken = 0
-
-        return self.currentState
-
-    @property
-    def stateSpace(self) -> int:
-        return len(states)
-
-    @property
-    def allActions(self) -> set:
-        return set([a for actions in self.actions.values() for a in actions])
-
-    @property
-    def actionSpace(self) -> int:
-        return len(self.allActions)  # To set to remove duplicate actions
-
-    def getActions(self) -> list:
-        return self.actions[self.currentState]
-
-    def step(self, action):
-        possibleOutcomes = self.transitions[self.currentState][action]  # {s: chance, s: chance, etc.}
-
-        newState = choices(list(possibleOutcomes.keys()), possibleOutcomes.values())[0]
-        reward = self.rewards[self.currentState][action][newState]
-
-        self.currentState = newState
-        self.stepsTaken += 1
-
-        return (newState,
-                reward,
-                newState not in transitions,  # In our case we treat 'sinks' (states with no further transitions) as terminal.
-                self.stepsTaken >= self.T,
-                None)  # What should info be?
-
-    def toGraph(self):
-        G = nx.DiGraph()
-
-        G.add_nodes_from(self.states, node_shape='s', color='blue')
-        G.add_nodes_from(self.allActions, node_shape='o', color='yellow')
-
-        # G.add_edges_from(self.actions)
-
-        plt.show()
-
-
-# Terminals + rested and tired (one per level)
-states = [f"R-{i}" for i in range(1, 6)] + [f"R-{i}" for i in range(1, 6)] + ["Dead", "Won"]
-actions = {f'R-{i}': ['attack', 'defend', 'train'] for i in range(1, 6)}
-actions.update({f'T-{i}': ['attack', 'rest'] for i in range(1, 6)})
-
-transitions = {f'R-{i}': {'attack': {'Won': -0.1 + 0.2 * i, 'Dead': 1.1 - 0.2 * i},
-                          'defend': {f'T-{i}': 0.3, f'R-{i}': 0.7},  # Boss has a 30% chance to attack.
-                          'train': {f'T-{i + 1}': 0.8, 'Dead': 0.2}
-                          } for i in range(1, 6)}
-transitions.update({f'T-{i}': {'rest': {f'R-{i}': 0.7, 'Dead': 0.3},
-                               'attack': {'Won': -0.05 + 0.1 * i, 'Dead': 1.05 - 0.1 * i}} for i in range(1, 6)})
-
 L_0 = 0
 L_1 = 1
 L_2 = 2
@@ -683,22 +611,10 @@ def GPI():
     for i in range(1):
         # 100 sweeps are done per iteration for policy evaluation
         policyEvaluation()
-        print (vTable[0])
-        print (vTable[1])
-        print (vTable[2])
-        print (vTable[3])
-        print (vTable[4])
-            
         # get q values of the current policy after evaluation
         for level in range(len(vTable)):
             for status in range(len(vTable[level])):
                 actionValues[level][status] = policyImprovement(level, status)
-        
-        print (actionValues[0])
-        print (actionValues[1])
-        print (actionValues[2])
-        print (actionValues[3])
-        print (actionValues[4])
 
         initialPolicy = updatePolicy()
 
